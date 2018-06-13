10ee771e6f470c2259b3c99b33fd8c8bb4f39be8
richard.poole1990@gmail.com <richard.poole1990@gmail.com>
2018-06-13 08:54:54 -0400

    4 working functions

--- scripts.js
+++ scripts.js
@@ -1,13 +1,49 @@
-var button = document.getElementById('carpetButton'); //
+var carpetInfo = document.getElementById('carpetButton'); 
+var chemInfo = document.getElementById('chemButton');
+var hogInfo = document.getElementById('hogButton');
+var treeInfo = document.getElementById('treeButton')
 
-button.onclick = function() {
-    var div = document.getElementById("carpet");
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
+// When carpet python button is clicked, adds HTML information into DOM
+carpetInfo.onclick = function() {
+    var infoShow = document.getElementById('info');
+    if (infoShow.style.display == 'block') {
+        infoShow.style.display = 'none';      
+      }
     else {
-        div.style.display = 'block';
-    }
+        infoShow.style.display = 'block';
+        infoShow.innerHTML = "test";
+       }
 };
 
-console.log("test");
+chemInfo.onclick = function() {
+    var infoShow = document.getElementById('info');
+    if (infoShow.style.display == 'block') {
+        infoShow.style.display = 'none';      
+      }
+    else {
+        infoShow.style.display = 'block';
+        infoShow.innerHTML = "test";
+       }
+};
+
+hogInfo.onclick = function() {
+    var infoShow = document.getElementById('info');
+    if (infoShow.style.display == 'block') {
+        infoShow.style.display = 'none';      
+      }
+    else {
+        infoShow.style.display = 'block';
+        infoShow.innerHTML = "test";
+       }
+};
+
+treeInfo.onclick = function() {
+    var infoShow = document.getElementById('info');
+    if (infoShow.style.display == 'block') {
+        infoShow.style.display = 'none';      
+      }
+    else {
+        infoShow.style.display = 'block';
+        infoShow.innerHTML = "test";
+       }
+};

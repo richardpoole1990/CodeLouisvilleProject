eedbbf3bd7b5aa7d40dd5a628fca7870e6b3c209
richard.poole1990@gmail.com <richard.poole1990@gmail.com>
2018-07-12 19:53:26 -0400

    almost complete

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 Website Project for GitHub
 
 Site made to help sell snakes for local breeder. After site loads, user can click buttons, which then uses JS to add HTML information depending on what button was selected.
+
+Requirements:
+
+Your project is responsive to different devices and/or browser sizes and improves the user experience in some way based on what type of device the project is viewed from - My site moves buttons row with text under, to column with text under.
+
+Your project includes at least 3 custom CSS class and/or ID selectors which are applied to your HTML elements - Currently sitting at 14 disregarding media queries. 
+
+
+Your project includes custom JavaScript including at least one JavaScript function - I only have one function that is repeated 4 times, but it has an even listener that when detects a button press, changes display type of an empty div to display block, and adds a blurb of information to innerHTML depending on what button was pressed.
+
+
+Your code has some comments noting major portions of your code: Comments added to anything I had issues getting to work. 
+
+
+Your project code is on your GitHub account in its own repository - https://github.com/richardpoole1990/CodeLouisvilleProject
+
+
+You are highly encouraged to include a front-end framework such as AJAX, jQuery, or Angular in your project as a demonstration of your ability to utilize these. However, this is not a requirement of a passing project. I did not use any of these -.-
+
+
+You are also highly encouraged to add some form of user interactivity to your project where something responds to an action the user performs. However, this is also not a requirement of passing. Button presses add HTML to web page.
+
+
+Have fun! Your project should be something you're proud of and that adequately demonstrates your base knowledge in the concepts you've learned. - I had a blast (except for one CSS issue.....)
+
+
+
+
--- scripts.js
+++ scripts.js
@@ -1,75 +1,75 @@
 //When buttons are clicked, manipulates DOM to display information base on which button is selected
 
 //Grabs button element and sets them to variable
-var carpetInfo = document.getElementById('carpetInfo'); 
-var chemInfo = document.getElementById('chemInfo');
-var hogInfo = document.getElementById('hogInfo');
-var treeInfo = document.getElementById('treeInfo');
+var carpetInfo = document.getElementById('carpetInfo'); //
+var chemInfo = document.getElementById('chemInfo');     // Grabs element id's for each button, and set's a variable
+var hogInfo = document.getElementById('hogInfo');       //
+var treeInfo = document.getElementById('treeInfo');     //
+var infoShow = document.getElementById('info');
+var temperment = document.getElementById('temperment');
+var husbandry = document.getElementById('husbandry'); 
+var diet = document.getElementById('diet');          // Grabs blank div for adding HTML later
 
 // When carpet python button is clicked, adds HTML information into DOM
-carpetInfo.onclick = function() {
-    var infoShow = document.getElementById('info');
-    if (infoShow.style.display == 'block') {
-        infoShow.style.display = 'none';
-        infoShow.innerHTML = " ";  
+carpetInfo.onclick = function() {                       //Listens for button click
+    if (infoShow.style.display == 'none') {        //Checks current style display, and if block
+        infoShow.style.display = 'none';                // change to none
+        infoShow.innerHTML = " ";                       // blank out HTML
        }
     else {
-        infoShow.style.display = 'block';
+        infoShow.style.display = 'flex';               // otherwise, display is block, and add below HTML
         infoShow.innerHTML = "<img id='python' class='speciesPic' src='Images/python.jpeg' alt='Close up of pythons head'>"
         infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        infoShow.innerHTML += "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        infoShow.innerHTML += "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        infoShow.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
+        temperment.innerHTML = "<strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        husbandry.innerHTML = "<strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        diet.innerHTML = "<strong>Diet:</strong> Frozen mice or rats.";
        }
 };
 
 // When Chemeleon button is clicked, adds HTML information into DOM
 chemInfo.onclick = function() {
-    var infoShow = document.getElementById('info');
-    if (infoShow.style.display == 'block') {
+    if (infoShow.style.display == 'none') {
         infoShow.style.display = 'none';
         infoShow.innerHTML = " ";      
         }
     else {
-        infoShow.style.display = 'block';
+        infoShow.style.display = 'flex';
         infoShow.innerHTML = "<img id='chem' class='speciesPic' src='Images/chem.jpg' alt='Chemeleon on branch'>"
-        infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        infoShow.innerHTML += "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        infoShow.innerHTML += "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        infoShow.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
+        infoShow.innerHTML += "Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
+        temperment.innerHTML += "<strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        husbandry.innerHTML += "<strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        diet.innerHTML += "<strong>Diet:</strong> Frozen mice or rats.";
         }
 };
 
 // When carpet Hognose is clicked, adds HTML information into DOM
 hogInfo.onclick = function() {
-    var infoShow = document.getElementById('info');
-    if (infoShow.style.display == 'block') {
+    if (infoShow.style.display == 'none') {
         infoShow.style.display = 'none';
         infoShow.innerHTML = " ";       
       }
     else {
-        infoShow.style.display = 'block';
+        infoShow.style.display = 'flex';
         infoShow.innerHTML = "<img class='speciesPic' id='hog' src='Images/Hog.jpg' alt='Hognose in tank'>"
         infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        infoShow.innerHTML += "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        infoShow.innerHTML += "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        infoShow.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
+        temperment.innerHTML = "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        husbandry.innerHTML = "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        diet.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
         }
 };
 
 // When Tree Pythong button is clicked, adds HTML information into DOM
 treeInfo.onclick = function() {
-    var infoShow = document.getElementById('info');
-    if (infoShow.style.display == 'block') {
+    if (infoShow.style.display == 'none') {
         infoShow.style.display = 'none';
         infoShow.innerHTML = " ";       
       }
     else {
-        infoShow.style.display = 'block';
+        infoShow.style.display = 'flex';
         infoShow.innerHTML = "<img id='gtp' class='speciesPic' src='Images/gtp.jpeg' alt='Green Tree Python on branch'>"
         infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        infoShow.innerHTML += "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        infoShow.innerHTML += "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        infoShow.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
+        temperment.innerHTML = "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        husbandry.innerHTML = "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        diet.innerHTML = "<br><strong>Diet:</strong> Frozen mice or rats.";
        }
 };

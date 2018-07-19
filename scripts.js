9cdaeb223d05c60dba1b96cf96bc5cf4936d4283
richard.poole1990@gmail.com <richard.poole1990@gmail.com>
2018-07-19 18:51:54 -0400

    attempted to add some more responsiveness. Fixed button issue.

--- scripts.js
+++ scripts.js
@@ -10,60 +10,73 @@ var temperment = document.getElementById('temperment');
 var husbandry = document.getElementById('husbandry'); 
 var diet = document.getElementById('diet');          // Grabs blank div for adding HTML later
 
+
 // When carpet python button is clicked, adds HTML information into DOM
 carpetInfo.onclick = function() {                       //Listens for button click
-    if (infoShow.style.display == 'none') {        //Checks current style display, and if block
+    if (infoShow.style.display == 'flex') {        //Checks current style display, and if block
         infoShow.style.display = 'none';                // change to none
         infoShow.innerHTML = " ";                       // blank out HTML
+        temperment.innerHTML = " ";
+        husbandry.innerHTML = " ";
+        diet.innerHTML = " ";                       
        }
     else {
         infoShow.style.display = 'flex';               // otherwise, display is block, and add below HTML
         infoShow.innerHTML = "<img id='python' class='speciesPic' src='Images/python.jpeg' alt='Close up of pythons head'>"
         infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        temperment.innerHTML = "<strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        husbandry.innerHTML = "<strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        diet.innerHTML = "<strong>Diet:</strong> Frozen mice or rats.";
+        infoShow.innerHTML += "<strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        infoShow.innerHTML += "<strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        infoShow.innerHTML += "<strong>Diet:</strong> Frozen mice or rats.";
        }
 };
 
 // When Chemeleon button is clicked, adds HTML information into DOM
 chemInfo.onclick = function() {
-    if (infoShow.style.display == 'none') {
-        infoShow.style.display = 'none';
-        infoShow.innerHTML = " ";      
-        }
+    if (infoShow.style.display == 'flex') {
+        infoShow.style.display = 'none';                // change to none
+        infoShow.innerHTML = " ";                       // blank out HTML
+        temperment.innerHTML = " ";
+        husbandry.innerHTML = " ";
+        diet.innerHTML = " ";                       
+       }
     else {
         infoShow.style.display = 'flex';
-        infoShow.innerHTML = "<img id='chem' class='speciesPic' src='Images/chem.jpg' alt='Chemeleon on branch'>"
-        infoShow.innerHTML += "Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
-        temperment.innerHTML += "<strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
-        husbandry.innerHTML += "<strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        diet.innerHTML += "<strong>Diet:</strong> Frozen mice or rats.";
-        }
+        infoShow.innerHTML = "<img class='speciesPic' id='chem' src='Images/chem.jpg' alt='Hognose in tank'>"
+        infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
+        temperment.innerHTML = "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
+        husbandry.innerHTML = "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
+        diet.innerHTML = "<br><strong>Diet:</strong> Frozen mice or rats.";
+       }
 };
 
 // When carpet Hognose is clicked, adds HTML information into DOM
 hogInfo.onclick = function() {
-    if (infoShow.style.display == 'none') {
-        infoShow.style.display = 'none';
-        infoShow.innerHTML = " ";       
-      }
+    if (infoShow.style.display == 'flex') {
+        infoShow.style.display = 'none';                // change to none
+        infoShow.innerHTML = " ";                       // blank out HTML
+        temperment.innerHTML = " ";
+        husbandry.innerHTML = " ";
+        diet.innerHTML = " ";                       
+       }
     else {
         infoShow.style.display = 'flex';
         infoShow.innerHTML = "<img class='speciesPic' id='hog' src='Images/Hog.jpg' alt='Hognose in tank'>"
         infoShow.innerHTML += "<br>Semiarboreal snakes inhabiting Queensland Australia, Jungle Carpets are a popular breed to keep for pets.";
         temperment.innerHTML = "<br><strong>Temperment:</strong> Aside from a strong feeding response, docile and easy to handle.";
         husbandry.innerHTML = "<br><strong>Husbandry:</strong> Temps should be around 90\xB0 Ferenheit. Humidity should be between 40% and 60%. Some type of branch for the snake to perch on should be provided. Recommended substrate would be dirt, though newspaper and tile are common.";
-        diet.innerHTML += "<br><strong>Diet:</strong> Frozen mice or rats.";
+        diet.innerHTML = "<br><strong>Diet:</strong> Frozen mice or rats.";
         }
 };
 
 // When Tree Pythong button is clicked, adds HTML information into DOM
 treeInfo.onclick = function() {
-    if (infoShow.style.display == 'none') {
-        infoShow.style.display = 'none';
-        infoShow.innerHTML = " ";       
-      }
+    if (infoShow.style.display == 'flex') {
+        infoShow.style.display = 'none';                // change to none
+        infoShow.innerHTML = " ";                       // blank out HTML
+        temperment.innerHTML = " ";
+        husbandry.innerHTML = " ";
+        diet.innerHTML = " ";                       
+       }
     else {
         infoShow.style.display = 'flex';
         infoShow.innerHTML = "<img id='gtp' class='speciesPic' src='Images/gtp.jpeg' alt='Green Tree Python on branch'>"
